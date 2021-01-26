029bff9184a23d27b8b9b5fba1e543c9397e6774
cemehov <46017113+cemehov@users.noreply.github.com>
2021-01-26 17:12:35 +0400

    Update sort_student.py

--- sort_student.py
+++ sort_student.py
@@ -2,8 +2,6 @@ def find_top_20(candidates):
     for i in candidates:
         sum_bals = i["scores"]["math"] + i["scores"]["russian_language"] + i["scores"]["computer_science"] + i["extra_scores"]
         i["sum_bals"] = sum_bals
-
-        
     candidates = sorted(candidates, key=lambda student: (student["sum_bals"], student["scores"]["math"], student["scores"]["computer_science"]), reverse=True)
     return [i["name"] for i in candidates[:20]]
     
